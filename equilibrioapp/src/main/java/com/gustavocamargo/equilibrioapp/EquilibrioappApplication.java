cb58cf6cded6417a0b936d1bde0c8b02e0338ce2
GustavoCamargodaSilva <gustavocamargodasilva@hotmail.com>
2025-03-30 13:21:36 -0300

    initial commit project

---
+++ equilibrioapp/src/main/java/com/gustavocamargo/equilibrioapp/EquilibrioappApplication.java
@@ -0,0 +1,13 @@
+package com.gustavocamargo.equilibrioapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquilibrioappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquilibrioappApplication.class, args);
+	}
+
+}
---
+++ equilibrioapp/src/test/java/com/gustavocamargo/equilibrioapp/EquilibrioappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavocamargo.equilibrioapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquilibrioappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
